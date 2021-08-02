06127d33097999eb5a977d91e861cdb71c1b5e6b
unknown <pablorua182@gmail.com>
2021-08-02 12:27:41 -0500

    re-up

--- src/config/server.js
+++ src/config/server.js
@@ -7,14 +7,14 @@ const middlewares = require('middlewares');
 
 const app=express();
 
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'../app/views'));
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-dotenv.config({path: path.join(__dirname,'../env/.envH,.env')})
+dotenv.config({path: path.join(__dirname,'../env/.env')})
 
 app.use('/resources', express.static(path.join(__dirname,'../public')));
 
