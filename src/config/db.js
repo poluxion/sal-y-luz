3a28da541209d2b2a45c5205dbb4559972d92424
unknown <pablorua182@gmail.com>
2021-08-05 20:36:04 -0500

    asistente y registro de evento reparadosv6

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,40 @@
 const mysql= require('mysql');
 
-const connection = mysql.createConnection({
-	host: "us-cdbr-east-04.cleardb.com",
-	user: "b58be2487761ea",
-	database: "heroku_0124097ebb4b235",
-	password: "b2fe26fe",
+const conexion_bd = {
+	host:"us-cdbr-east-04.cleardb.com",
+	user:"b58be2487761ea",
+	database:"heroku_0124097ebb4b235",
+	password:"b2fe26fe"
+};
+
+function handleDisconnect(conexion_bd) {
+	connection = mysql.createPool(conexion_bd);
+
+	connection.getConnection(function(err){
+		if (err) {
+			console.log('error al conectarse a la bd: ', err);
+			setTimeout(handleDisconnect, 2000);
+		}
+	});
+
+	connection.on('error', function(err) {
+		console.log('error en la db', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+handleDisconnect(conexion_bd);
+module.exports = connection;
+
+
+/*const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	database: process.env.DB_DATABASE
 });
 
 connection.connect((err)=>
@@ -15,5 +45,4 @@ connection.connect((err)=>
 		return;
 	}
 	console.log("Conectado exitosamente a la BD");
-});
-module.exports = connection;
+});*/
