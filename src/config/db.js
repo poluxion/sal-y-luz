206d8f85f28cd06c672b2bce259d82834015f17e
unknown <pablorua182@gmail.com>
2021-08-05 20:06:50 -0500

    asistente y registro de evento reparadosv5

--- src/config/db.js
+++ src/config/db.js
@@ -1,36 +1,19 @@
 const mysql= require('mysql');
 
-const conexion_bd = {
+const connection = mysql.createConnection({
 	host: "us-cdbr-east-04.cleardb.com",
 	user: "b58be2487761ea",
 	database: "heroku_0124097ebb4b235",
-	password: "b2fe26fe"
-};
-/*const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	database: process.env.DB_DATABASE
-});*/
+	password: "b2fe26fe",
+});
 
-function handleDisconnect(conexion_bd){
-	connection = mysql.createPool(conexion_bd);
-
-
-	connection.getConnection(function(err) {
-		if (err){
-			console.log('error al conectar a la bd: ',err);
-			setTimeout(handleDisconnect, 2000);
-		}
-	});
-
-	connection.on('error', function(err){
-		console.log('db error', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			handleDisconnect();
-		}else{
-			throw err;
-		}
-	});
-}
-handleDisconnect(conexion_bd);
-//module.exports = conexion_bd;
+connection.connect((err)=>
+{
+	if (err) 
+	{
+		console.log("El error de conexion bd es: "+err)
+		return;
+	}
+	console.log("Conectado exitosamente a la BD");
+});
+module.exports = connection;
